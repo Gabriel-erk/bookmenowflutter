15fed4fcc0b17bbc93c3627c903b171e9f2716a5
Gabriel Erick <gabrielmmve13@gmail.com>
2025-02-12 22:26:05 -0300

    tela serviços

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bookmenowflutter/controller/autenticacao_controller.dart';
 import 'package:bookmenowflutter/view/tela_login.dart';
+import 'package:bookmenowflutter/view/tela_servico.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,8 +9,10 @@ void main() {
     // chamando o provider que vai controlar a modificação de estado do nosso app
     MultiProvider(
       // controlador que vai faze a modificação de estado do nosso app
-      providers: [ ChangeNotifierProvider(create: (_) => AutenticacaoController())],
-      child: MainApp(),
+      providers: [
+        ChangeNotifierProvider(create: (_) => AutenticacaoController())
+      ],
+      child: const MainApp(),
     ),
   );
 }
--- lib/view/tela_login.dart
+++ lib/view/tela_login.dart
@@ -1,4 +1,5 @@
 import 'package:bookmenowflutter/controller/autenticacao_controller.dart';
+import 'package:bookmenowflutter/view/tela_servico.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,6 +14,8 @@ class _TelaLoginEstado extends State<TelaLogin> {
   // váriaveis que terão os valores que serão passados para os campos do formulário
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _senhaController = TextEditingController();
+  // váriavel responsável por controlar o formulário (irá ficar olhando para ver o formulário quando o usuário digitar algo)
+  final GlobalKey<FormState> _chaveFormulario = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +25,10 @@ class _TelaLoginEstado extends State<TelaLogin> {
         padding: const EdgeInsets.all(16.0),
         // formulário
         child: Form(
+          key: _chaveFormulario,
           // column me permite organizar os widgets em colunas, e vários por conta do children
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // todo textFormField tem um controller
               TextFormField(
@@ -33,24 +38,46 @@ class _TelaLoginEstado extends State<TelaLogin> {
                   labelText: 'E-mail',
                 ),
                 keyboardType: TextInputType.emailAddress,
+                // validação do campo com função anônima, onde (valor! nunca será nullo, uso ! para dizer isso), se valor!.isEmpty (ou seja, se a váriavel valor for nulla) mostra o texto "informe um e-mail valido" se não, retorna nullo, ou seja, não mostra msg nenhuma
+                validator: (valor) =>
+                    valor!.isEmpty ? "Informe um e-mail válido" : null,
               ),
               TextFormField(
                 controller: _senhaController,
                 decoration: const InputDecoration(labelText: 'Senha'),
                 obscureText: true, // esconde a senha
+                validator: (valor) =>
+                    valor!.isEmpty ? "Informe uma senha válida" : null,
+              ),
+              const SizedBox(
+                height: 30,
               ),
               // se estiver carregando chama circularprogress (mostra um circulo enquanto carrega), se não chama o botão
               autenticacaoController.carregando
-                  ? CircularProgressIndicator()
+                  ? const LinearProgressIndicator()
                   : ElevatedButton(
                       onPressed: () async {
-                        bool sucesso = await autenticacaoController.acessar(
-                            _emailController.text, _senhaController.text);
+                        //currentState!.validate() vai olhar todos os meus campos com validação e se todos estiverem válidos, ele não vai fazer nada, se algum campo não estiver válido , ele vai mostrar a msg de erro presente no validator de cada textFormField (chamando eles no onPressed pois não quero que consulte minha API para verificar se possue um registro com e-mail e senha vazios, q não é possivel, então se estiver assim nem olha minha api)
+                        if (_chaveFormulario.currentState!.validate()) {
+                          bool sucesso = await autenticacaoController.acessar(
+                              _emailController.text, _senhaController.text);
 
-                        if (sucesso) {
-                          print("conectado");
-                        } else {
-                          print("erro no e-mail ou senha");
+                          if (sucesso) {
+                            // push chama uma nova tela (pop volta)
+                            // context é o contexto da minha tela, é a tela que estou agora (telaLogin), context é o context de 'buildCOntext' no parametro widget build lá no topo do código
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const TelaServico()),
+                            );
+                          } else {
+                            // se não der para logar (caso digite um e-mail e senha que não existam), aparece uma mini barra (tipo um alert) dizendo que "erro ao fazer o login"
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("E-mail ou senha inválido"),
+                              ),
+                            );
+                          }
                         }
                       },
                       child: const Text("Entrar"),
